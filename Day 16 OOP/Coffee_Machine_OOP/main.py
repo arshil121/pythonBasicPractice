33c27f72cb5882a7be36705ce9a51a5afe905de5
shoab-alam <arshilalam121@gmail.com>
2024-06-18 11:50:56 +0600

    added

--- Day 16 OOP/Coffee_Machine_OOP/main.py
+++ Day 16 OOP/Coffee_Machine_OOP/main.py
@@ -1,23 +1,22 @@
-from menu import Menu
+from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-money_machine = MoneyMachine()
-coffee_maker = CoffeeMaker()
 menu = Menu()
-
-is_on = True
-
-while is_on:
-    options = menu.get_items()
-    choice = input(f"What would you like? ({options}): ")
+coffee_maker = CoffeeMaker()
+money_mchine = MoneyMachine()
+turn_on = True
+while turn_on:
+    choice = input(f"What would you like? ({menu.get_items()}): ").lower()
     if choice == "off":
-        is_on = False
+        print("Machine Turned Off!!")
+        turn_on = False
     elif choice == "report":
         coffee_maker.report()
-        money_machine.report()
+        money_mchine.report()
     else:
         drink = menu.find_drink(choice)
-        
-        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
-          coffee_maker.make_coffee(drink)
+        if coffee_maker.is_resource_sufficient(drink) and money_mchine.make_payment(
+            drink.cost
+        ):
+            coffee_maker.make_coffee(drink)
